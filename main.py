f0543eeb610a8c3b0f89cc70ba32fbea6b4d0acf
BuRny <gamingburny@gmail.com>
2020-03-27 03:01:54 +0100

    Fix season min

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ class MMRranges:
         function_call_count = 0
         for region, season_max in self.season_numbers.items():
             # Only show the last 8 seasons
-            season_min = max(season_max - 8, self.season_min)
+            season_min = max(season_max - 7, self.season_min)
             for season_number in range(season_min, season_max + 1):
                 for queue_id in self.queue_ids:
                     for team_type in self.team_types:
