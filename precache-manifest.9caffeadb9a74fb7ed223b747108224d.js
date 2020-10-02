f11c8bfc98cae1db4ef68efb548a203a2f112e3c
BurnySc2 <gamingburny@gmail.com>
2020-10-02 05:56:05 +0000

    Deploying to gh-pages from master @ a16ef9bce01c42a5e070a555e7b6d4fcbe26263a 🚀

---
+++ precache-manifest.9caffeadb9a74fb7ed223b747108224d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c7da1e48f6b0c643fc3e18e71f49d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6bf0a06a1da459225196",
+    "url": "./static/css/main.3436458e.chunk.css"
+  },
+  {
+    "revision": "09dbdd1a10d267c25d6a",
+    "url": "./static/js/2.f10fff75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f10fff75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf0a06a1da459225196",
+    "url": "./static/js/main.836c8595.chunk.js"
+  },
+  {
+    "revision": "d55f7a57a44c6849556e",
+    "url": "./static/js/runtime-main.6f6d36f4.js"
+  }
+]);
